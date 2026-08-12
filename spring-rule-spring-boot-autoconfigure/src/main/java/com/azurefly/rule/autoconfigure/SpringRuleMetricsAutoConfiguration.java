5b91522bb05076378a0fe746238817eed13fa853
SuPan <azure_dream@foxmail.com>
2026-08-12 10:47:58 +0800

    Lazily resolve optional MeterRegistry

--- spring-rule-spring-boot-autoconfigure/src/main/java/com/azurefly/rule/autoconfigure/SpringRuleMetricsAutoConfiguration.java
+++ spring-rule-spring-boot-autoconfigure/src/main/java/com/azurefly/rule/autoconfigure/SpringRuleMetricsAutoConfiguration.java
@@ -2,8 +2,8 @@ package com.azurefly.rule.autoconfigure;
 
 import com.azurefly.rule.core.RuleEngineListener;
 import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -11,13 +11,12 @@ import org.springframework.context.annotation.Bean;
 
 @AutoConfiguration(before = SpringRuleAutoConfiguration.class)
 @ConditionalOnClass(MeterRegistry.class)
-@ConditionalOnBean(MeterRegistry.class)
 @ConditionalOnProperty(prefix = "spring.rule.metrics", name = "enabled", havingValue = "true", matchIfMissing = true)
 public class SpringRuleMetricsAutoConfiguration {
 
     @Bean
     @ConditionalOnMissingBean(MicrometerRuleEngineListener.class)
-    public RuleEngineListener micrometerRuleEngineListener(MeterRegistry meterRegistry) {
-        return new MicrometerRuleEngineListener(meterRegistry);
+    public RuleEngineListener micrometerRuleEngineListener(ObjectProvider<MeterRegistry> meterRegistries) {
+        return new MicrometerRuleEngineListener(meterRegistries);
     }
 }
