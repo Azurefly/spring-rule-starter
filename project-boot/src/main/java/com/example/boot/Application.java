8a8e3cf9bf15be6a17e174a8e79f840f7075c70f
SuPan <azure_dream@foxmail.com>
2026-08-12 10:38:51 +0800

    Disable unused generated Spring Security user

--- project-boot/src/main/java/com/example/boot/Application.java
+++ project-boot/src/main/java/com/example/boot/Application.java
@@ -1,18 +1,17 @@
-
 package com.example.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {"com.example"})
+@SpringBootApplication(
+        scanBasePackages = {"com.example"},
+        excludeName = {"org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration"})
 @EnableJpaRepositories(basePackages = {"com.example.api.repository"})
 @EntityScan(basePackages = {"com.example.api.entity"})
-@ComponentScan(basePackages = {"com.example"})
 public class Application {
-    
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
